4cc1f8d5eea11f9f589f7e753e99b5a40e0082d2
Tabita99 <tabita.m@hotmail.co.uk>
2020-05-23 22:31:40 +0000

    width of logo changed for 768px and above

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
+//landing page
+
 $ (document).ready(function () {
 
 $(".grid-column").addClass("visibility");
 $("#game-title").hide().fadeIn(3000);
-$("#logo").hide().delay(1500).fadeIn(3000).fadeOut();
+//$("#logo").hide().delay(1500).fadeIn(3000).fadeOut();
 
  
 });
